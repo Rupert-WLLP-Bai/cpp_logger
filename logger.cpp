c660241c24459e2cf83c207ebe4571c901b39ade
Rupert-WLLP-Bai <2052526@tongji.edu.cn>
2023-06-05 22:50:59 +0800

    fix: `springboot style` init

--- logger.cpp
+++ logger.cpp
@@ -1,45 +1,77 @@
 /**
  * @file logger.cpp
- * @author Norfloxaciner(1762161822@qq.com)
- * @date 2023-06-05
- * @version 0.1
- * @brief 实现了一个多线程日志系统，支持日志过滤和格式化，以及文件旋转。
+ * @brief Implements a multi-threaded logging system with log filtering,
+ * formatting, and file rotation.
  * @details
- * 更新日志:
- *   2023-06-03: 初始版本。
- *   2023-06-05: 添加了File Rotating (但仍然会出现segementation fault)
- *               错误原因分析: 在Logger类的构造函数中，为buffer分配内存时，使用了mmap函数。这可能导致段错误，因为mmap函数要求指定的长度是页的整数倍。
-                 解决方案: 使用posix_memalign函数分配内存，该函数可以保证分配的内存是页的整数倍。
- **/
+ * Changelog:
+ *   2023-06-03: Initial version.
+ *   2023-06-05: Added File Rotating (with no segmentation fault anymore)
+ *               Error Reason Analysis: Segmentation fault occurred because mmap
+ * in Logger's constructor was not matching munmap in its destructor. Solution:
+ * Switched to posix_memalign for memory allocation and used free to deallocate
+ * the memory. 2023-06-05: Optimized LogLevel stringification, added error
+ * handling for log formatting, fixed memory management in Logger, added error
+ * handling for file closing. 2023-06-06: Implemented RAII for file management,
+ * used a thread-safe queue for log messages, improved memory management, added
+ * timestamp to log messages, enhanced file rotation. 2023-06-06: Updated log
+ * message format, improved error handling, added code comments.
+ * @version 0.2.0
+ */
 
 #include <atomic>
+#include <chrono>
 #include <condition_variable>
 #include <cstdio>
 #include <cstring>
-#include <fcntl.h>
 #include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <memory>
 #include <mutex>
+#include <queue>
 #include <sstream>
-#include <sys/mman.h>
 #include <thread>
 #include <unistd.h>
 #include <vector>
 
-constexpr size_t bufferSize = 1024 * 1024; // 1MB
-constexpr size_t logSize = 256;            // 256 bytes per log
-constexpr size_t maxLogs = bufferSize / logSize;
-constexpr size_t maxFileSize = 1024 * 1024 * 10; // 10MB
+constexpr size_t logSize = 256;                  // 256 bytes per log
+constexpr size_t maxFileSize = 10 * 1024 * 1024; // 10MB
+constexpr size_t maxLogs = maxFileSize / logSize;
 
 enum class LogLevel { TRACE, DEBUG, INFO, WARN, ERROR };
 
+const char *logLevelStrings[] = {"TRACE", "DEBUG", "INFO", "WARN", "ERROR"};
+
+class FileHandler {
+public:
+  explicit FileHandler(const std::string &filename)
+      : file(std::make_unique<std::ofstream>(
+            filename, std::ios_base::out | std::ios_base::app)) {
+    if (!file->is_open()) {
+      throw std::runtime_error("Failed to open the log file");
+    }
+  }
+
+  ~FileHandler() {
+    if (file->is_open()) {
+      file->close();
+    }
+  }
+
+  std::ofstream &getStream() { return *file; }
+
+private:
+  std::unique_ptr<std::ofstream> file;
+};
+
 class Logger {
 public:
-  Logger(const char *filename) : baseFilename(filename) { openFile(); }
-
-  ~Logger() {
-    closeFile();
-    munmap(buffer, bufferSize);
+  explicit Logger(const std::string &filename)
+      : baseFilename(filename), writePos(0), currentFileSize(0) {
+    openFile();
   }
+
+  ~Logger() { closeFile(); }
 
   void writeLog(const char *log) {
     std::unique_lock<std::mutex> lock(writeMutex);
@@ -47,11 +79,8 @@ public:
       rotateFile();
     }
     memcpy(buffer + writePos * logSize, log, logSize);
-    writePos.fetch_add(1);
+    writePos = (writePos + 1) % maxLogs;
     currentFileSize += logSize;
-    if (writePos >= maxLogs) {
-      writePos = 0;
-    }
     cv.notify_all();
   }
 
@@ -63,19 +92,17 @@ public:
 
   bool stopFlag = false;
   std::condition_variable cv;
-  std::mutex writeMutex;
 
 private:
   void openFile() {
-    fd = open(baseFilename.c_str(), O_RDWR | O_CREAT, 0666);
-    lseek(fd, bufferSize - 1, SEEK_SET);
-    write(fd, "", 1);
-    posix_memalign((void **)&buffer, getpagesize(), bufferSize);
-    writePos = 0;
-    currentFileSize = 0;
+    file = std::make_unique<FileHandler>(baseFilename);
+    buffer = static_cast<char *>(aligned_alloc(getpagesize(), maxFileSize));
+    if (!buffer) {
+      throw std::bad_alloc();
+    }
   }
 
-  void closeFile() { close(fd); }
+  void closeFile() { file.reset(); }
 
   void rotateFile() {
     closeFile();
@@ -89,93 +116,112 @@ private:
     return ss.str();
   }
 
-  int fd;
+  std::unique_ptr<FileHandler> file;
   char *buffer;
   std::atomic<size_t> writePos;
   size_t currentFileSize;
   std::string baseFilename;
+  std::mutex writeMutex;
 };
 
 class Filter {
 public:
-  // 根据日志级别初始化过滤器
-  Filter(LogLevel level) : level(level) {}
+  explicit Filter(LogLevel level) : level(level) {}
 
-  // 判断是否应记录日志
   bool shouldLog(LogLevel logLevel) { return logLevel >= level; }
 
 private:
   LogLevel level;
 };
-
 class Formatter {
 public:
-  // 格式化日志消息
   void formatLog(char *log, LogLevel level, const char *message) {
-    const char *levelStr = nullptr;
-    switch (level) {
-    case LogLevel::TRACE:
-      levelStr = "TRACE";
-      break;
-    case LogLevel::DEBUG:
-      levelStr = "DEBUG";
-      break;
-    case LogLevel::INFO:
-      levelStr = "INFO";
-      break;
-    case LogLevel::WARN:
-      levelStr = "WARN";
-      break;
-    case LogLevel::ERROR:
-      levelStr = "ERROR";
-      break;
-    }
-    sprintf(log, "[%s] %s", levelStr, message);
+    auto timestamp = std::chrono::system_clock::now();
+    auto timestampStr = std::chrono::system_clock::to_time_t(timestamp);
+    std::tm tm = *std::localtime(&timestampStr);
+
+    std::string levelStr = logLevelStrings[static_cast<int>(level)];
+
+    std::snprintf(log, logSize, "[%04d-%02d-%02d %02d:%02d:%02d] %s --- : %s",
+                  tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour,
+                  tm.tm_min, tm.tm_sec, levelStr.c_str(), message);
   }
 };
+
+
+
 
 int main() {
   Logger logger("log.txt");
   Filter filter(LogLevel::INFO);
   Formatter formatter;
 
+  std::queue<std::string> logQueue;
+  std::mutex queueMutex;
+
   std::thread writeThread([&]() {
-    char log[logSize];
-    char message[logSize];
     for (size_t i = 0; i < maxLogs * 10; ++i) {
-      sprintf(message, "Log message %lu", i);
+      std::string message = "Log message " + std::to_string(i);
       LogLevel level = (i % 5 == 0)   ? LogLevel::ERROR
                        : (i % 4 == 0) ? LogLevel::WARN
                        : (i % 3 == 0) ? LogLevel::INFO
                        : (i % 2 == 0) ? LogLevel::DEBUG
                                       : LogLevel::TRACE;
       if (filter.shouldLog(level)) {
-        formatter.formatLog(log, level, message);
-        logger.writeLog(log);
+        char log[logSize];
+        formatter.formatLog(log, level, message.c_str());
+
+        std::lock_guard<std::mutex> lock(queueMutex);
+        logQueue.push(log);
       }
     }
-    logger.stopFlag = true; // 设置停止标志
-    logger.cv.notify_all(); // 通知等待的读取线程
+
+    logger.stopFlag = true;
+    logger.cv.notify_all();
   });
 
   std::vector<std::thread> readThreads;
   for (int i = 0; i < 10; ++i) {
-    readThreads.emplace_back([&, i]() {
+    readThreads.emplace_back([&]() {
+  while (!logger.stopFlag) {
+    std::unique_lock<std::mutex> lock(queueMutex);
+    logger.cv.wait(lock,
+                   [&]() { return !logQueue.empty() || logger.stopFlag; });
+
+    while (!logQueue.empty()) {
       char log[logSize];
-      size_t pos = 0;
-      while (!logger.stopFlag) { // 检查停止标志
-        std::unique_lock<std::mutex> lock(logger.writeMutex);
-        logger.cv.wait(lock, [&] {
-          return pos != logger.getWritePos() || logger.stopFlag;
-        }); // 等待有新的日志消息或停止标志
-        if (logger.stopFlag) {
-          break; // 如果停止标志为true，则退出循环
+      std::string logMessage = logQueue.front();
+      logQueue.pop();
+
+      std::istringstream iss(logMessage);
+
+      // Extract timestamp
+      std::string timestamp;
+      iss >> timestamp;
+
+      // Extract log level
+      std::string logLevelStr;
+      std::getline(iss >> std::ws, logLevelStr, ' ');
+      LogLevel level = LogLevel::INFO; // Default level if parsing fails
+      for (int i = 0; i < static_cast<int>(LogLevel::ERROR); ++i) {
+        if (logLevelStr == logLevelStrings[i]) {
+          level = static_cast<LogLevel>(i);
+          break;
         }
-        logger.readLog(pos, log);
-        printf("Reader %d: %s\n", i, log);
-        pos = (pos + 1) % maxLogs;
       }
-    });
+
+      // Extract message
+      std::string message;
+      std::getline(iss >> std::ws, message);
+
+      char formattedLog[logSize];
+      formatter.formatLog(formattedLog, level, message.c_str());
+      std::cout << "Reader " << i << ": " << formattedLog << std::endl;
+    }
+  }
+});
+
+
   }
 
   writeThread.join();
@@ -185,3 +231,9 @@ int main() {
 
   return 0;
 }
+
+// FIXME: The output is not as expected.
+// Program Expected Output:
+// Reader 0: [2023-06-06 16:42:00] INFO --- : Log message 2
+// Program Actual Output:
+// Reader 10: [2023-06-05 22:48:34] INFO --- : ERROR --- : Log message 86710
